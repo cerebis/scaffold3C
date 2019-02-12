54c60fbe51e8568e08b1a15c52b641e6da392cc6
Matthew DeMaere <matt.demaere@gmail.com>
2019-02-12 15:27:47 +1100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='scaffold3C',
+    description='Compute the order and orientation of assembly contigs using Hi-C sequencing data.',
+    long_description=long_description,
+    version='0.1a1',
+    author='Matthew Z DeMaere',
+    author_email='matt.demaere@gmail.com',
+    platforms='Linux-86_x64',
+    packages=setuptools.find_packages(),
+    url='https://github.com/cerebis/scaffold3C',
+    license='GNU Affero General Public License v3',
+
+    install_requires=['proxigenomics_toolkit'],
+
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: POSIX :: Linux',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Development Status :: 2 - Pre-Alpha'
+    ]
+
+)
